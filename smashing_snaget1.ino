ab454672208043b4b3baa4a52f96b4939ac6ff53
wardamukhtarkhan <102410054+wardamukhtarkhan@users.noreply.github.com>
2022-04-02 13:55:34 +0500

    Add files via upload
    design a circuit with 5 leds

---
+++ smashing_snaget1.ino
@@ -0,0 +1,47 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(0, OUTPUT);
+  pinMode(1,OUTPUT);
+   pinMode(2, OUTPUT);
+  pinMode(3,OUTPUT);
+   pinMode(4,OUTPUT);
+}
+
+
+void loop()
+{
+  digitalWrite(0, HIGH);
+  delay(500); 
+  digitalWrite(0,LOW);
+  delay(500); 
+  digitalWrite(1, HIGH);
+  delay(500); 
+  digitalWrite(1,LOW);
+  delay(500);
+   digitalWrite(2, HIGH);
+  delay(500); 
+  digitalWrite(2,LOW);
+  delay(500); 
+  digitalWrite(3, HIGH);
+  delay(500);
+  digitalWrite(3,LOW);
+  delay(500);
+   digitalWrite(4, HIGH);
+  delay(500);
+  digitalWrite(4,LOW);
+  delay(500);
+   digitalWrite(3, HIGH);
+  delay(500); 
+  digitalWrite(3,LOW);
+  delay(500); 
+  digitalWrite(2, HIGH);
+  delay(500); 
+  digitalWrite(2,LOW);
+  delay(500);
+   digitalWrite(1, HIGH);
+  delay(500); 
+  digitalWrite(1,LOW);
+  delay(500); 
+}
